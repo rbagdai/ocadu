c20405ce000bc1dbd1356b926441553362e24832
rbagdai <bagdai.rahul@gmail.com>
2019-11-07 01:49:00 -0500

    Add files via upload

---
+++ scripts/web-verbs-script.js
@@ -0,0 +1,50 @@
+var greetings = "Hello, Rahul!"
+greetings = greetings + " And welcome everyone!"
+
+$(".mainghost").click(function() {
+  $(this)
+    .addClass("ghost")
+
+})
+
+$(".ghostTwo").click(function() {
+  $(this)
+    .addClass("ghostTwoAnimate")
+
+})
+
+$(".ghostThree").click(function() {
+  $(this)
+    .addClass("ghostThreeAnimate")
+
+})
+
+$(".ghostFour").click(function() {
+  $(this)
+    .addClass("ghostFourAnimate")
+
+})
+
+$(".cloudOne").click(function() {
+  $(this)
+    .addClass("cloudOneClick")
+
+})
+
+$(".cloudTwo").click(function() {
+  $(this)
+    .addClass("cloudTwoClick")
+
+})
+
+$(".cloudThree").click(function() {
+  $(this)
+    .addClass("cloudThreeClick")
+
+})
+
+$(".sun").click(function() {
+  $(this)
+    .addClass("sunClick")
+
+})
